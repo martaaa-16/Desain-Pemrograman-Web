38c8f07d6858a4c8932a31c0ddb822757f3bd1aa
martaaa-16 <martaprama@gmail.com>
2025-10-21 11:52:59 +0700

    praktikum 1 soal 5

--- JOBSHEET 8-PHP FORM UPLOAD, COOKIES, DAN SESSION/upload.php
+++ JOBSHEET 8-PHP FORM UPLOAD, COOKIES, DAN SESSION/upload.php
@@ -1,15 +1,17 @@
 <?php
 if (isset($_POST["submit"])) {
-    $targetdir= "uploads/";
+    $targetdir = "uploads/";
     $targetfile = $targetdir . basename($_FILES["myfile"]["name"]);
     $fileType = strtolower(pathinfo($targetfile, PATHINFO_EXTENSION));
 
     $allowedExtensions = array("jpg", "jpeg", "png", "gif");
-    $maxsize = 5*1024*1024;
-    
-    if (in_array($fileType, $allowedExtensions) && $_FILES["myfile"]["size"]<=$maxsize) {
+    $maxsize = 5 * 1024 * 1024;
+
+    if (in_array($fileType, $allowedExtensions) && $_FILES["myfile"]["size"] <= $maxsize) {
         if (move_uploaded_file($_FILES["myfile"]["tmp_name"], $targetfile)) {
             echo "File berhasil diunggah.";
+            echo "<h3>Pratinjau Gambar:</h3>";
+            echo "<img src='$targetfile' width='200' style='height:auto; border:1px solid #ccc; margin-top:10px;'>";
         } else {
             echo "Gagal mengunggah file.";
         }
