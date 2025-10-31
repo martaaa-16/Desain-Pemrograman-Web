c11a32acf2143d945ea0dd1c8973a99e940d2550
martaaa-16 <martaprama@gmail.com>
2025-10-31 17:52:31 +0700

    praktikum 9

---
+++ JOBSHEET 9/sessionDel.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE htmk>
+<html>
+
+<body>
+    <?php
+    session_unset();
+    session_destroy();
+    echo "All session variables are now removed, and the session is destroyed.";
+    ?>
+</body>
+
+</html>
