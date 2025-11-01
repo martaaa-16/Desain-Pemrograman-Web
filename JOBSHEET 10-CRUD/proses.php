182263b62e540b180ffe3ffe7bc73cefdbe8424f
martaaa-16 <martaprama@gmail.com>
2025-11-01 18:16:54 +0700

    praktikum 3

--- JOBSHEET 10-CRUD/proses.php
+++ JOBSHEET 10-CRUD/proses.php
@@ -8,7 +8,8 @@ $alamat = $_POST['alamat'];
 $no_telp = $_POST['no_telp'];
 
 if ($aksi == 'tambah') {
-    $query = "INSERT INTO anggota (nama, jenis_kelamin, alamat, no_telp) VALUES ('$nama', '$jenis_kelamin', '$alamat', '$no_telp')";
+    $query = "INSERT INTO anggota (nama, jenis_kelamin, alamat, no_telp) 
+              VALUES ('$nama', '$jenis_kelamin', '$alamat', '$no_telp')";
 
     if (mysqli_query($koneksi, $query)) {
         header("Location: index.php");
@@ -16,6 +17,25 @@ if ($aksi == 'tambah') {
     } else {
         echo "Gagal menambahkan data: " . mysqli_error($koneksi);
     }
+
+} elseif ($aksi == 'ubah') {
+    if (isset($_POST['id'])) {
+        $id = $_POST['id'];
+
+        $query = "UPDATE anggota 
+                  SET nama='$nama', jenis_kelamin='$jenis_kelamin', alamat='$alamat', no_telp='$no_telp' 
+                  WHERE id='$id'";
+
+        if (mysqli_query($koneksi, $query)) {
+            header("Location: index.php");
+            exit();
+        } else {
+            echo "Gagal mengupdate data: " . mysqli_error($koneksi);
+        }
+    } else {
+        echo "ID tidak valid.";
+    }
 }
+
 mysqli_close($koneksi);
 ?>
