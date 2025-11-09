f83971ab18967ebc52cb447e1f5b7a4c15e898b2
martaaa-16 <martaprama@gmail.com>
2025-11-09 21:15:30 +0700

    praktikum 6 soal no 6.3

--- JOBSHEET 10-CRUD/csrf.php
+++ JOBSHEET 10-CRUD/csrf.php
@@ -8,7 +8,7 @@
 
     $headers = apache_request_headers();
     if (isset($headers['Csrf-Token'])) {
-        if ($headers['Csrf-Token'] !== $_SESSION['csrf-token']) {
+        if ($headers['Csrf-Token'] !== $_SESSION['csrf_token']) {
             exit(json_encode(['error' => 'Wrong CSRF token.']));
         }
     } else {
--- JOBSHEET 10-CRUD/index.php
+++ JOBSHEET 10-CRUD/index.php
@@ -7,21 +7,25 @@ include "auth.php";
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="csrf-token" content="<?= isset($_SESSION['csrf_token']) ? $_SESSION['csrf_token'] : '' ?>">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet"
-        crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css">
+    <!--- Csrf Token -->
+    <meta name="csrf-token" content="<?= $_SESSION['csrf_token'] ?>">
+    <!--- Bootstrap -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.css" rel="stylesheet">
+    <!--- Font Awesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
+        integrity="sha384- fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <!--- Data Tables -->
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css">
     <title>Data Anggota</title>
 </head>
 
 <body>
     <nav class="navbar navbar-dark bg-primary">
-        <a class="navbar-brand" href="index.php" style="color: white;">CRUD Dengan Ajax</a>
+        <a class="navbar-brand" href="index.php" style="color: #fff;">CRUD Dengan Ajax</a>
     </nav>
 
-    <div class="container" style="margin: 30px;">
-        <h2>Data Anggota</h2>
+    <div class="container">
+        <h2 align="center" style="margin: 30px;">Data Anggota</h2>
         <form method="post" class="form-data" id="form-data">
             <div class="row">
                 <div class="col-sm-9">
@@ -37,122 +41,98 @@ include "auth.php";
                     <div class="form-group">
                         <label>Jenis Kelamin:</label><br>
                         <input type="radio" name="jenis_kelamin" id="jenkel1" value="L" required="true"> Laki-laki
-                        <input type="radio" name="jenis_kelamin" id="jenkel2" value="P" required="true"> Perempuan
+                        <input type="radio" name="jenis_kelamin" id="jenkel2" value="P"> Perempuan
                     </div>
                     <p class="text-danger" id="err_jenis_kelamin"></p>
                 </div>
+            </div>
 
-                <div class="col-sm-12"> 
-                    <div class="form-group">
-                        <label>Alamat:</label>
-                        <textarea name="alamat" id="alamat" class="form-control" required="true"></textarea>
-                        <p class="text-danger" id="err_alamat"></p>
-                    </div>
-                </div>
+            <div class="form-group">
+                <label>Alamat:</label>
+                <textarea name="alamat" id="alamat" class="form-control" required="true"></textarea>
+                <p class="text-danger" id="err_alamat"></p>
+            </div>
 
-                <div class="col-sm-12">
-                    <div class="form-group">
-                        <label>No Telepon:</label>
-                        <input type="number" name="no_telp" id="no_telp" class="form-control" required="true">
-                        <p class="text-danger" id="err_no_telp"></p>
-                    </div>
-                </div>
+            <div class="form-group">
+                <label>No Telepon:</label>
+                <input type="number" name="no_telp" id="no_telp" class="form-control" required="true">
+                <p class="text-danger" id="err_no_telp"></p>
+            </div>
 
-                <div class="col-sm-12">
-                    <div class="form-group">
-                        <button type="button" name="simpan" id="simpan" class="btn btn-primary">
-                            <i class="fa fa-save"></i> Simpan
-                        </button>
-                    </div>
-                </div>
+            <div class="form-group">
+                <button type="button" name="simpan" id="simpan" class="btn btn-primary">
+                    <i class="fa fa-save"></i> Simpan
+                </button>
             </div>
         </form>
         <hr>
         <div class="data"></div>
     </div>
 
-    <div class="text-center">
-        <p>&copy; <?= date('Y'); ?> | Desain Dan Pemrograman Web |
-            <a href="https://google.com/">Google</a>
-        </p>
+    <div class="text-center"> <?php echo date('Y'); ?> Copy right:
+        <a href="https://google.com/">Desain Dan Pemrograman Web </a>
     </div>
 
+    <!--- JQuery -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <!--- DataTable -->
     <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
 
     <script type="text/javascript">
         $(document).ready(function () {
-            // Load data saat halaman pertama kali dibuka
             $.ajaxSetup({
                 headers: {
                     'Csrf-Token': $('meta[name="csrf-token"]').attr('content')
                 }
             });
 
-            // 1. Load data anggota ke dalam div.data
             $('.data').load('data.php');
 
-            // 2. Logika Simpan Data (termasuk Validasi)
-            $("#simpan").click(function() {
+            $("#simpan").click(function () {
                 var data = $(".form-data").serialize();
-                var jenkel1 = document.getElementById("jenkel1").checked;
-                var jenkel2 = document.getElementById("jenkel2").checked;
+                var jenkel1 = document.getElementById("jenkel1").value;
+                var jenkel2 = document.getElementById("jenkel2").value;
                 var nama = document.getElementById("nama").value;
                 var alamat = document.getElementById("alamat").value;
                 var no_telp = document.getElementById("no_telp").value;
 
-                // --- Validasi Input ---
-                var isValid = true;
-
-                // Validasi Nama
                 if (nama == "") {
                     document.getElementById("err_nama").innerHTML = "Nama Harus Diisi";
-                    isValid = false;
                 } else {
                     document.getElementById("err_nama").innerHTML = "";
                 }
 
-                // Validasi Alamat
                 if (alamat == "") {
                     document.getElementById("err_alamat").innerHTML = "Alamat Harus Diisi";
-                    isValid = false;
                 } else {
                     document.getElementById("err_alamat").innerHTML = "";
                 }
 
-                // Validasi Jenis Kelamin
                 if (jenkel1 == false && jenkel2 == false) {
                     document.getElementById("err_jenis_kelamin").innerHTML = "Jenis Kelamin Harus Dipilih";
-                    isValid = false;
                 } else {
                     document.getElementById("err_jenis_kelamin").innerHTML = "";
                 }
 
-                // Validasi No Telepon
                 if (no_telp == "") {
                     document.getElementById("err_no_telp").innerHTML = "No Telepon Harus Diisi";
-                    isValid = false;
                 } else {
                     document.getElementById("err_no_telp").innerHTML = "";
                 }
 
-                // --- Pengiriman Data via AJAX jika semua valid ---
-                if (isValid) {
+                if (nama != "" && alamat != "" && (document.getElementById("jenkel1").checked == true || document.getElementById("jenkel2").checked == true) && no_telp != "") {
                     $.ajax({
                         type: 'POST',
-                        url: 'form_action.php', // Asumsi file PHP yang memproses simpan/update
+                        url: 'form_action.php',
                         data: data,
-                        success: function(response) {
-                            // Muat ulang tabel data setelah sukses
+                        success: function () {
                             $('.data').load('data.php');
-                            
-                            // Kosongkan form input
                             document.getElementById("id").value = "";
                             document.getElementById("form-data").reset();
                         },
-                        error: function(response) {
+                        error: function (response) {
                             console.log(response.responseText);
                         }
                     });
