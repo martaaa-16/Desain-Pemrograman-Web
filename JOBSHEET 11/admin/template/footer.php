350e199bfb538454b80a76f65894460fa54985fd
martaaa-16 <martaprama@gmail.com>
2025-11-10 14:54:58 +0700

    praktikum 3 soal no. 3.1

---
+++ JOBSHEET 11/admin/template/footer.php
@@ -0,0 +1,12 @@
+<html>
+
+<body>
+    <script src="assets/dist/js/bootstrap.bundle.min.js"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@4.3.2/dist/chart.umd.js"
+        integrity="sha384-eI7PSr3L1XLISH8JdDlI5YN/njoSsxfbkrCTnJrzXt+ENPSMOVBXbD+l6sEG4ZoLP"
+        crossorigin="anonymous"></script>
+    <script src="bootstrap-5.3.2-examples/assets/custom/dashboard.js"></script>
+</body>
+
+</html>
