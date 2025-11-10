ba773c2fde6d1960e1a0cbace0aaff85b459292a
martaaa-16 <martaprama@gmail.com>
2025-11-10 16:45:47 +0700

    praktikum 5 soal no. 5.2

--- JOBSHEET 11/fungsi/hapus.php
+++ JOBSHEET 11/fungsi/hapus.php
@@ -4,6 +4,7 @@ if (!empty($_SESSION['username'])) {
     require '../config/koneksi.php';
     require '../fungsi/pesan_kilat.php';
     require '../fungsi/anti_injection.php';
+
     if (!empty($_GET['jabatan'])) {
         $id = antiinjection($koneksi, $_GET['id']);
         $query = "DELETE FROM jabatan WHERE id = '$id'";
@@ -14,6 +15,22 @@ if (!empty($_SESSION['username'])) {
         }
         header("Location: ../index.php?page=jabatan");
 
+    } elseif (!empty($_GET['anggota'])) {
+        $id = antiinjection($koneksi, $_GET['id']);
+
+        $query = "DELETE FROM user WHERE id = '$id'";
+
+        if (mysqli_query($koneksi, $query)) {
+            $query2 = "DELETE FROM anggota WHERE user_id = '$id'";
+            if (mysqli_query($koneksi, $query2)) {
+                pesan('success', "Anggota Telah Terhapus.");
+            } else {
+                pesan('warning', "Data Login Terhapus Tetapi Data Anggota Tidak Terhapus Karena: " . mysqli_error($koneksi));
+            }
+        } else {
+            pesan('danger', "Anggota Tidak Terhapus Karena: " . mysqli_error($koneksi));
+        }
+        header("Location: ../index.php?page=anggota");
     }
 }
 ?>
